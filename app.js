7aefbef59f1b850c2dcb5ec73c31a53be2510100
kchettiar1 <krayativ.kc@gmail.com>
2022-03-18 16:01:16 +0000

    updated menu button + nav items interaction

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
+// Menu Button and Animation
 menuBtn = document.querySelector('.fas');
+let menuOpen = false;
+
+addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.remove('fa-bars')
+    menuBtn.classList.add('fa-times')
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('fa-times')
+    menuBtn.classList.add('fa-bars')
+    menuOpen = false;
+  }
+})
+
 navItems = document.querySelector('.nav-items');
+let navOpen = false;
 
-function menuToggle() {
-  menuBtn.classList.toggle('fa-bars')
-  menuBtn.classList.toggle('fa-times')
-  navItems.classList.toggle('hidden')
-}
-
-menuBtn.addEventListener('click', menuToggle);
-
-
+addEventListener('click', () => {
+  if (!navOpen) {
+    navItems.classList.remove('collapse')
+    navItems.classList.add('expand')
+    navOpen = true;
+  } else {
+    navItems.classList.remove('expand')
+    navItems.classList.add('collapse')
+    navOpen = false;
+  }
+})
