b3eba643614810af227fef26b6125738b2947484
kchettiar1 <krayativ.kc@gmail.com>
2022-03-20 14:20:11 +0000

    menu animation expand/collapse

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-// Menu Button and Animation
-menuBtn = document.querySelector('.fas');
+// Menu-toggle
+const menuBtn = document.querySelector('.menu-btn')
 let menuOpen = false;
 
-addEventListener('click', () => {
+
+menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
-    menuBtn.classList.remove('fa-bars')
-    menuBtn.classList.add('fa-times')
-    menuOpen = true;
+    menuBtn.classList.add('open');
+    menuOpen = true
   } else {
-    menuBtn.classList.remove('fa-times')
-    menuBtn.classList.add('fa-bars')
+    menuBtn.classList.remove('open');
     menuOpen = false;
+
   }
+  console.log('menuBtn',menuBtn);
+  console.log('menuOpen',menuOpen);
 })
 
+// Nav Menu Animation
 navItems = document.querySelector('.nav-items');
 let navOpen = false;
 
 addEventListener('click', () => {
   if (!navOpen) {
-    navItems.classList.remove('collapse')
-    navItems.classList.add('expand')
+    navItems.classList.remove('collapse');
+    navItems.classList.add('expand');
     navOpen = true;
   } else {
-    navItems.classList.remove('expand')
-    navItems.classList.add('collapse')
+    navItems.classList.remove('expand');
+    navItems.classList.add('collapse');
     navOpen = false;
   }
 })
